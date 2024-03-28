3f02851ff8e4969031608e3c3d71b5447ff05bb3
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2024-03-28 15:28:36 +0530

    update shopkeeperOrders.jsx

--- frontend/App/Screens/RegisterScreen/ShopkeeperOrders/ShopkeeperOrders.jsx
+++ frontend/App/Screens/RegisterScreen/ShopkeeperOrders/ShopkeeperOrders.jsx
@@ -273,7 +273,7 @@ const styles = StyleSheet.create({
 	productImage: {
         width: 100,
         height: 100,
-        borderRadius: 10,
+        borderRadius: 50,
         marginRight: 10,
     },
     productButton: {
