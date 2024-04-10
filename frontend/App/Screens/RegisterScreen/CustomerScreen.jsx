aedfdf51ce326c7d8e2b99b22b6adf5719240d16
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2024-04-10 11:10:15 +0530

    update

--- frontend/App/Screens/RegisterScreen/CustomerScreen.jsx
+++ frontend/App/Screens/RegisterScreen/CustomerScreen.jsx
@@ -50,7 +50,7 @@ export default function CustomerScreen({ route, onFormSubmit }) {
         }
     
         // Call the API to register the user
-        fetch('http://192.168.29.68:3000/register', {
+        fetch('http://172.16.16.141:3000/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
